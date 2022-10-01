7383c900cd64f68ae39efb33574bfe5923577830
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-01 12:07:04 -0300

    Update exe.1 - Q.4: Escalas.py

--- exe.1 - Q.4: Escalas.py
+++ exe.1 - Q.4: Escalas.py
@@ -1,21 +1,49 @@
-temp1 = float(input('Informe o valor da 1º temperatura: '))
-temp_1 = str(input('Informe se a escala da 1º temperatura é C ou F: '))
-
-tuplinha = (temp1, temp_1)
-
-temp2 = float(input('Informe o valor da 2º temperatura: '))
-temp_2 = str(input('Informe se a escala da 2º temperatura é C ou F: '))
-
-tuplinha_2 = (temp2, temp_2)
-
-def maior(tuplinha, tuplinha_2):
-     if tuplinha[1] != tuplinha_2[1]:
-        if tuplinha_2 == 'F':
-            far = (tuplinha[0] * 1.8) + 32
+def maior(tupla1, tupla2):
+    if tupla1[1] == tupla2[1]:
+        if tupla2[1] == 'F':
+            if tupla2[0] > tupla1[0]:
+                print(f'{tupla2[0]}°F é maior que {tupla1[0]}°F')
             
-        elif tuplinha_2 == 'C':
-            cel = (tuplinha[0] - 32) / 1.8
-     return far
+            elif tupla1[0] > tupla2[0]:
+                print(f'{tupla1[0]}°F é maior que {tupla2[0]}°F')
+
+        elif tupla2[1] == 'C':
+            if tupla2[0] > tupla1[0]:
+                print(f'{tupla2[0]}°C é maior que {tupla1[0]}°C')
+            
+            elif tupla1[0] > tupla2[0]:
+                print(f'{tupla1[0]}°C é maior que {tupla2[0]}°C')
+    
+    
+    if tupla1[1] != tupla2[1]:
+         if tupla2[1] == 'F':
+            far = (tupla1[0] * 1.8) + 32
+            if far > tupla2[0]:
+                print(f'{far:.1f}°F é maior que {tupla2[0]}°F')
+            elif tupla2[0] > far:
+                print(f'{tupla2[0]}°F é maior que {far:.1f}°F')
+
+         elif tupla2[1] == 'C':
+            cel = (tupla1[0] - 32) / 1.8
+            if cel > tupla2[0]:
+                print(f'{cel:.1f}°C é maior que {tupla2[0]}°C')
+            elif tupla2[0] > cel:
+                print(f'{tupla2[0]}°C é maior que {cel:.1f}°C')
 
 
-maior(tuplinha, tuplinha_2)
+
+
+temp1 = float(input('Qual o valor da 1º temperatura?: '))
+print()
+esc1 = str(input('Qual a escala da 1º temperatura?(C ou F): '))
+print()
+tupla1 = (temp1, esc1)
+
+temp = float(input('Qual o valor da 2º temperatura?: '))
+print()
+esc = str(input('Qual a escala da 2º temperatura?(C ou F): '))
+print()
+tupla2 = (temp, esc)
+
+print(maior())
+print()
