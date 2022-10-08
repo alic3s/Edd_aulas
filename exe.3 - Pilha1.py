1be1e62fdf5e2a6a68e800a33bb506f6c3ea4ee2
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-08 18:53:58 -0300

    Update exe.3 - Pilha1.py

--- exe.3 - Pilha1.py
+++ exe.3 - Pilha1.py
@@ -1,24 +1,27 @@
 #Verificação de expressão aritmética
 
-def verificar(self, entrada):
-    pilha = Pilha()
+def verifica(entrada):
+    pilha_s = Pilha()
     for s in entrada:
         if s == '(' or s == '[' or s == '{':
-            pilha.insere(s)
+            pilha_s.insere(s)
         elif s == ')' or s == ']' or s == '}':
-            if (pilha.vazia()):
+            if (pilha_s.vazia()):
                 return False
-            if s == ')' and pilha.remove != '(':
+            if s == ')' and pilha_s.remove != '(':
                 return False
-            if s == ']' and pilha.remove != '[':
+            elif s == ']' and pilha_s.remove != '[':
                 return False
-            if s == '}' and pilha.remove != '{':
+            elif s == '}' and pilha_s.remove != '{':
                 return False
-    if pilha.vazia():
+
+    if pilha_s.vazia():
         return True
     else:
         return False
 
+
 #as expressoes sao: [(x + y) * 2] = 0; [(x + y] * 2) = 0; [(x + y * 2) = 0
 
-string = (list(input('Digite a expresão: ')))
+string=list(input("Digite expressão: "))
+print(verifica(string))
