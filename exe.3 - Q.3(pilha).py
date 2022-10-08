bec791423770d88823d16968dc095b3fc189a123
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-08 18:13:04 -0300

    Update exe.3 - Q.3(pilha).py

--- exe.3 - Q.3(pilha).py
+++ exe.3 - Q.3(pilha).py
@@ -4,18 +4,27 @@
 
 import re
 
-frase = input('Informe a frase: ')
+frase = str(input('Informe a frase: '))
 pilha = Pilha()
 
 if frase[len(frase) - 1] == '.':
+    
     frase = re.sub('[,.!?]', '', frase)
+    
     palavras = frase.split(' ')
+    
     for palavra in palavras:
+        
         for letra in palavra:
+            
             pilha.insere(letra)
-            string = ''
+            
+        string = ''
+        
             while not pilha.vazia():
+                
                 string += pilha.remove()
-                print(string, end = ' ')
+                
+            print(string, end = ' ')
 else:
     print('A frase não termina com .')
