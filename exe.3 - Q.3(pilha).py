dff95fb304178a56fb96aad039256dac3c032faa
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-08 15:28:41 -0300

    Update exe.3 - Q.3(pilha).py

--- exe.3 - Q.3(pilha).py
+++ exe.3 - Q.3(pilha).py
@@ -1,23 +1,5 @@
-# primeira opção
-import re
+#a opção com lista está no classroom
 
-def inverte(palavra):
-    if re.match(r'^\w+$',palavra):
-        return palavra[::-1]
-    return palavra
-
-frase = (str(input('Digite uma frase: ')))
-
-invertida = ''.join(inverte(palavra) for palavra in re.split(r'(\w+)',frase))
-print(invertida)
-
-
-frase = input('Digite uma frase: ')
-print('Você digitou: {}'.format(frase))
-invertida = ' '.join(palavra[::-1] for palavra in frase.split())
-print('A frase que você digitou invertida fica: {}'.format(invertida))
-
-#segunda opção
 import re
 
 frase = input('Informe a frase: ')
@@ -35,5 +17,3 @@ if frase[len(frase) - 1] == '.':
                 print(string, end = ' ')
 else:
     print('A frase não termina com .')
-   
-#terceira opção está no classroom
