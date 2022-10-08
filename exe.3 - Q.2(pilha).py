c80c3a767f817119eed5a3cc4ddbdebaa3689cfe
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-08 15:23:42 -0300

    Create exe.3 - Q.2(pilha).py

---
+++ exe.3 - Q.2(pilha).py
@@ -0,0 +1,2 @@
+#Considere uma pilha que armazena caracteres. Faça uma função para determinar se uma string é da forma XY, onde X é uma cadeia formada por caracteres
+#arbitrários e Y é o reverso de X. Por exemplo, se x = ABCD, então y = DCBA. Considere que x e y são duas strings distintas.
