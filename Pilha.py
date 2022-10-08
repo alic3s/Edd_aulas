bf2117aa01034bfd446b4e7fc612bb77e87ef6b8
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-08 15:33:05 -0300

    Update Pilha.py

--- Pilha.py
+++ Pilha.py
@@ -46,27 +46,3 @@ print(pilha.tamanho)
 while pilha.topo != None:
     pilha.remove()
     print('Removendo o elemento que está no topo da pilha: ', pilha)
-
-
-def verificar(self, entrada):
-    pilha = Pilha()
-    for s in entrada:
-        if s == '(' or s == '[' or s == '{':
-            pilha.insere(s)
-        elif s == ')' or s == ']' or s == '}':
-            if (pilha.vazia()):
-                return False
-            if s == ')' and pilha.remove != '(':
-                return False
-            if s == ']' and pilha.remove != '[':
-                return False
-            if s == '}' and pilha.remove != '{':
-                return False
-    if pilha.vazia():
-        return True
-    else:
-        return False
-
-#as expressoes sao: [(x + y) * 2] = 0; [(x + y] * 2) = 0; [(x + y * 2) = 0
-
-string = (list(input('Digite a expresão: ')))
