34545afd12744a847d88fb81bec1721fd52fdff4
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-08 17:11:54 -0300

    Update exe.4 - Q1(fila).py

--- exe.4 - Q1(fila).py
+++ exe.4 - Q1(fila).py
@@ -1,1 +1,2 @@
-#socorro
+#Considere uma pilha P vazia e uma fila F não vazia. Utilizando apenas fila e pilha vazias, e as operações insere e remove. 
+#Escreva uma função que inverta a ordem doselementos da fila.
