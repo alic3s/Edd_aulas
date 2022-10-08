80e51e7e42e9f129f18de894b2ac1462460a60ff
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-08 15:33:57 -0300

    Update exe.3 - Pilha1.py

--- exe.3 - Pilha1.py
+++ exe.3 - Pilha1.py
@@ -1,1 +1,24 @@
-#aipapai
+#Verificação de expressão aritmética
+
+def verificar(self, entrada):
+    pilha = Pilha()
+    for s in entrada:
+        if s == '(' or s == '[' or s == '{':
+            pilha.insere(s)
+        elif s == ')' or s == ']' or s == '}':
+            if (pilha.vazia()):
+                return False
+            if s == ')' and pilha.remove != '(':
+                return False
+            if s == ']' and pilha.remove != '[':
+                return False
+            if s == '}' and pilha.remove != '{':
+                return False
+    if pilha.vazia():
+        return True
+    else:
+        return False
+
+#as expressoes sao: [(x + y) * 2] = 0; [(x + y] * 2) = 0; [(x + y * 2) = 0
+
+string = (list(input('Digite a expresão: ')))
