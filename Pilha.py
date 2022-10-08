a5093ab2153af4e523e79d2b5ef1364b7fe572f7
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-08 15:27:04 -0300

    Update Pilha.py

--- Pilha.py
+++ Pilha.py
@@ -33,25 +33,6 @@ class Pilha:
         removido = self.topo.dado
         self.topo = self.topo.anterior
         return removido
-    
-    # questao 3
-    import re
-
-    frase = input('Informe a frase: ')
-    pilha = Pilha()
-
-    if frase[len(frase) - 1] == '.':
-        frase = re.sub('[,.!?]', '', frase)
-        palavras = frase.split(' ')
-        for palavra in palavras:
-            for letra in palavra:
-                pilha.insere(letra)
-            string = ''
-            while not pilha.vazia():
-                string += pilha.remove()
-            print(string, end = ' ')
-    else:
-        print('A frase não termina com .')
 
 
 pilha = Pilha()
