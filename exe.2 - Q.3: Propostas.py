3e2b7e0aa5e9b46c731d049d1513d18d6cd886ac
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-11 18:49:43 -0300

    Update exe.2 - Q.3: Propostas.py

--- exe.2 - Q.3: Propostas.py
+++ exe.2 - Q.3: Propostas.py
@@ -1,46 +1,45 @@
+#Um edital de concorrência pública avaliará 5 propostas considerando
+#três critérios: qualidade, preço e prazo. Cada um dos três recebe uma nota de zero
+#a dez. Escreva um programa que leia as notas de preço, prazo e qualidade de cada
+#proposta e escreva a nota geral de cada proposta bem como qual das propostas
+#apresentadas ganhou a concorrência (obteve maior nota). Para calcular as notas
+#deve-se utilizar os seguintes critérios: 1. Se a nota da qualidade for inferior a 7, a
+#nota geral será 0, independente dos outros fatores. 2- Se a qualidade for igual ou
+#superior a 7, e a nota do preço for igual ou superior a 7, então a nota será a média
+#das três notas. 3- Se a nota da qualidade for igual ou superior a 7 e a do preço for
+#inferior a 7, então a nota geral será será (qualidade+2*preço+prazo)/4.
+
+
 p1 = {1: 'Preço', 2: 'Prazo', 3: 'Qualidade'}
 p2 = {1: 'Preço', 2: 'Prazo', 3: 'Qualidade'}
-p3 = {1: 'Preço', 2: 'Prazo', 3: 'Qualidade'}
-p4 = {1: 'Preço', 2: 'Prazo', 3: 'Qualidade'}
-p5 = {1: 'Preço', 2: 'Prazo', 3: 'Qualidade'}
+
 
 p1[1] = (int(input('Informe a nota do preço: ')))
 p1[2] = (int(input('Informe a nota do prazo: ')))
 p1[3] = (int(input('Informe a nota da qualidade: ')))
 
-p2[1] = (int(input('Informe a nota do preço: ')))
+
+p2[1] = (int(input('\nInforme a nota do preço: ')))
 p2[2] = (int(input('Informe a nota do prazo: ')))
 p2[3] = (int(input('Informe a nota da qualidade: ')))
 
-p3[1] = (int(input('Informe a nota do preço: ')))
-p3[2] = (int(input('Informe a nota do prazo: ')))
-p3[3] = (int(input('Informe a nota da qualidade: ')))
+nota_geral1 = p1[1] + p1[2] + p1[3]
+nota_geral2 = p2[1] + p2[2] + p2[3]
 
-p4[1] = (int(input('Informe a nota do preço: ')))
-p4[2] = (int(input('Informe a nota do prazo: ')))
-p4[3] = (int(input('Informe a nota da qualidade: ')))
+print(f'A nota geral (sem critérios) da proposta foi:{nota_geral1}')
+print(f'A nota geral (sem critérios) da proposta foi: {nota_geral2}')
 
-p5[1] = (int(input('Informe a nota do preço: ')))
-p5[2] = (int(input('Informe a nota do prazo: ')))
-p5[3] = (int(input('Informe a nota da qualidade: ')))
 
-print('A nota geral da proposta foi: ', p1[1] + p1[2] + p1[3])
-print('A nota geral da proposta foi: ', p2[1] + p2[2] + p2[3])
-print('A nota geral da proposta foi: ', p3[1] + p3[2] + p3[3])
-print('A nota geral da proposta foi: ', p4[1] + p4[2] + p4[3])
-print('A nota geral da proposta foi: ', p5[1] + p5[2] + p5[3])
+if p1[3] < 7:
+    nota_geral1 = 0
+elif p1[3] >= 7 and p1[1] >= 7:
+    nota_geral1 = nota_geral1 / 3
+elif p1[3] >= 7 and p1[1] < 7:
+    nota_geral1 = (p1[3] + p1[1] + p1[2]) / 4
 
-max_key1 = max(p1, key = p1.get)
-print(max_key)
-
-max_key2 = max(p2, key = p2.get)
-print(max_key2)
-
-max_key3 = max(p3, key = p3.get)
-print(max_key3)
-
-max_key4 = max(p4, key = p4.get)
-print(max_key4)
-
-max_key5 = max(p5, key = p5.get)
-print(max_key5)
+print(nota_geral1)
+    
+if p1[1] + p1[2] + p1[3] > p2[1] + p2[2] + p2[3]:
+    print(f'A proposta com maior nota foi a primeira: {p1}')
+else:
+    print(f'A proposta com maior nota foi a segunda: {p2}')
