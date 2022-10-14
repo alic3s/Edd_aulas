4e430d62c7340df568e8aa72b67ac89043a49308
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-14 19:56:43 -0300

    Update exe.4 - Q1(fila).py

--- Fila/exe.4 - Q1(fila).py
+++ Fila/exe.4 - Q1(fila).py
@@ -1,2 +1,30 @@
 #Considere uma pilha P vazia e uma fila F não vazia. Utilizando apenas fila e pilha vazias, e as operações insere e remove. 
 #Escreva uma função que inverta a ordem doselementos da fila.
+
+from fila import Fila
+
+from pilha import Pilha
+
+Pilha1 = Pilha()
+
+Fila1 = Fila()
+
+Fila2 = Fila()
+
+Fila1.insere(7)
+Fila1.insere(5)
+Fila1.insere(3)
+
+print(Fila1)
+
+while not Fila1.vazia():
+  temp = Fila1.remove()
+  Pilha1.insere(temp)
+
+while not Pilha1.vazia():
+  temp2 = Pilha1.remove()
+  Fila2.insere(temp2)
+
+print(temp)
+print(temp2)
+print(Fila2)                              
