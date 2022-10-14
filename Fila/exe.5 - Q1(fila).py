f96bb87862bfa054bf765b9b3d604228d714ed2a
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-14 20:29:20 -0300

    Update exe.5 - Q1(fila).py

--- Fila/exe.5 - Q1(fila).py
+++ Fila/exe.5 - Q1(fila).py
@@ -1,1 +1,29 @@
+from fila import Fila
 
+def compara(F1, F2):
+  if F1.tamanho == F2.tamanho:
+    for v in range(F1.tamanho):
+      for v in range(F2.tamanho):
+        if F1 == F2:
+          print(F1, F2)
+
+
+
+
+
+
+
+F1 = Fila()
+
+for i in range(4):
+                                         F1.insere(i)
+print(F1)
+
+
+F2 = Fila()
+
+for i in range(4):
+                                         F2.insere(i)
+print(F2)
+
+compara(F1,F2)
