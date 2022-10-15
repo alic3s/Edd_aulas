1fc7d7a806f73a21409a1f46d50897a96ef7c919
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-14 21:27:45 -0300

    Update exe.5 - Q1(fila).py

--- Fila/exe.5 - Q1(fila).py
+++ Fila/exe.5 - Q1(fila).py
@@ -10,3 +10,20 @@ def compare(self, F2):
                 atual1 = atual1.proximo
                 atual2 = atual2.proximo
             return True
+
+F1 = Fila()
+
+F1.insere(1)
+F1.insere(2)
+F1.insere(3)
+F1.insere(4)
+
+print(F1)
+
+F2 = Fila()
+
+F2.insere(1)
+F2.insere(2)
+F2.insere(3)
+
+print(F1.compare(F2))
