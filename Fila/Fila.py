f1e985d6afa16030c313f46a594d3c7a57bbcc3d
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-14 21:23:37 -0300

    Update Fila.py

--- Fila/Fila.py
+++ Fila/Fila.py
@@ -40,19 +40,6 @@ class Fila:
             self.cauda = None
         return removido
 
-    def compare(self, F2):
-        if self.tamanho != F2.tamanho:
-            return False
-        else:
-            atual1 = self.cabeca
-            atual2 = F2.cabeca
-            while atual1 != None:
-                if atual1.dado != atual2.dado:
-                    return False
-                atual1 = atual1.proximo
-                atual2 = atual2.proximo
-            return True
-
 fila = Fila()
 
 for i in range(5):
