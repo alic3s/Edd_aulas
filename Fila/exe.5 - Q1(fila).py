0d11bf25aaabe02c0e750dd0b68f0108bb20d0ec
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-15 14:45:24 -0300

    Update exe.5 - Q1(fila).py

--- Fila/exe.5 - Q1(fila).py
+++ Fila/exe.5 - Q1(fila).py
@@ -1,15 +1,30 @@
+'''Questão 01. Dado duas filas F1 e F2 que armazenam números inteiros, faça um programa
+que tenha as seguintes funções:
++Uma função para testar se as duas filas são iguais (em tamanho e conteúdo). Caso
+não sejam iguais, em tamanho, informe qual fila é maior.
++Uma função para retornar o número de elementos de uma fila que possuem valor
+ımpar.
++Uma função para retornar o número de elementos da uma fila que possuem valor
+par.
+Obs. As operações devem ser realizadas sem alteração da fila (sem inserção e remoção).'''
+
 def compare(self, F2):
         if self.tamanho != F2.tamanho:
-            return False
+                if self.tamanho > F2.tamanho:
+                        return 'A fila 1 é a maior'
+                else:
+                        return 'A fila 2 é a maior'
         else:
-            atual1 = self.cabeca
-            atual2 = F2.cabeca
-            while atual1 != None:
-                if atual1.dado != atual2.dado:
-                    return False
-                atual1 = atual1.proximo
-                atual2 = atual2.proximo
-            return True
+                print('As filas possuem o mesmo tamanho')
+                atual1 = self.cabeca
+                atual2 = F2.cabeca
+                while atual1 != None:
+                        if atual1.dado != atual2.dado:
+                                return False
+                        atual1 = atual1.proximo
+                        atual2 = atual2.proximo
+                        return True
+                
 
 F1 = Fila()
 
