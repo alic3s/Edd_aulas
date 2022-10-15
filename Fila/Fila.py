2155e7d0dab9020c94993f7bdc6f2885d21a59d9
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-14 21:20:39 -0300

    Update Fila.py

--- Fila/Fila.py
+++ Fila/Fila.py
@@ -1,11 +1,12 @@
 class Nodo:
-    def __init__(self, dado = 0, proximo_nodo = None):
+    def __init__(self, dado=0, proximo_nodo=None):
         self.dado = dado
         self.proximo = proximo_nodo
-    
+
     def __repr__(self):
         return f'{self.dado} -> {self.proximo}'
-    
+
+
 class Fila:
     def __init__(self):
         self.cabeca = None
@@ -20,16 +21,17 @@ class Fila:
             return True
         else:
             return False
-    
+
     def insere(self, novo_dado):
         novo_nodo = Nodo(novo_dado)
+        self.tamanho += 1
         if self.vazia():
             self.cabeca = novo_nodo
             self.cauda = novo_nodo
         else:
             self.cauda.proximo = novo_nodo
             self.cauda = novo_nodo
-    
+
     def remove(self):
         removido = self.cabeca.dado
         self.cabeca = self.cabeca.proximo
@@ -37,6 +39,19 @@ class Fila:
         if self.cabeca == None:
             self.cauda = None
         return removido
+
+    def compare(self, F2):
+        if self.tamanho != F2.tamanho:
+            return False
+        else:
+            atual1 = self.cabeca
+            atual2 = F2.cabeca
+            while atual1 != None:
+                if atual1.dado != atual2.dado:
+                    return False
+                atual1 = atual1.proximo
+                atual2 = atual2.proximo
+            return True
 
 fila = Fila()
 
