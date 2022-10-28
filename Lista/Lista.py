d13c19178d795f97798bb70123bef994f3253c0c
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-28 12:07:01 -0300

    Update Lista.py

--- Lista/Lista.py
+++ Lista/Lista.py
@@ -23,7 +23,7 @@ class Lista:
     
     def insereInicio(self, novo_dado):
         novo_nodo = Nodo(novo_dado)
-        if self.vazia():
+        if self.vazia:
             self.cabeca = novo_nodo
             self.cauda = novo_nodo
         else:
@@ -33,24 +33,24 @@ class Lista:
     
     def insereFim(self, novo_dado):
         novo_nodo = Nodo(novo_dado)
-        if self.vazia():
+        if self.vazia:
             self.cabeca = novo_nodo
             self.cauda = novo_nodo
         else:
-            self.cauda.proximo = self.cauda
+            self.cauda.proximo = novo_nodo
             self.cauda = novo_nodo
         self.tamanho += 1
     
     def remove(self, posicao):
-        if self.vazia():
+        if self.vazia:
             return f'Impossível remover valor de lista vazia'
         elif (posicao) == 0:    #questão de projeto
                 removido = self.cabeca.dado
                 self.cabeca = self.cabeca.proximo
                 if self.cabeca == None:
                     self.cauda = None
-                    self.tamanho += 1
-                    return removido
+                self.tamanho -= 1
+                return removido
         else:
             aux = 0
             if (posicao >= self.tamanho):
@@ -64,12 +64,12 @@ class Lista:
                 removido = nodo_atual.dado
             if nodo_atual == self.cauda:
                 self.cauda = nodo_anterior
-                nodo_anterior.proximo = nodo_atual.proximo
-                self.tamanho -= 1
-                return removido
+            nodo_anterior.proximo = nodo_atual.proximo
+            self.tamanho -= 1
+            return removido
     
     def busca(self, dado):
-        if self.vazia():
+        if self.vazia:
             return 'Elemento não encontrado'
         else:
             nodo_atual = self.cabeca
@@ -79,3 +79,40 @@ class Lista:
                 else:
                     return i
             return 'Elemento não encontrado'
+    
+    '''def insere(self, novo_dado, posicao):
+        if 0 <= posicao <= self.tamanho:
+            if posicao == 0:
+                self.insereInicio(novo_dado)
+                self.tamanho += 1
+            elif posicao == (self.tamanho):
+                self.insereFim(novo_dado)
+                self.tamanho += 1
+            else:
+                nodo_atual = self.cabeca
+                i = 0
+                while i != posicao:
+                    nodo_anterior = nodo_atual
+                    nodo_atual = nodo_atual.proximo
+                    i += 1
+                removido = nodo_atual.dado
+                if nodo_atual == self.cauda:
+                    self.cauda = nodo_anterior
+                nodo_anterior.proximo = nodo_atual.proximo
+                self.tamanho -= 1'''
+        
+
+    def inserir(self, novo_dado, posicao):
+        posicao = posicao - 1
+        if ((posicao < 0) or (posicao > self.tamanho)):
+            print ("Posição inválida!")
+            return False
+        elif (posicao == 0):
+            self.inicio = Nodo(novo_dado, None)
+        else:
+            aux = self.inicio
+            for i in range (1, posicao):
+                aux = aux.proximo
+            aux.proximo = Nodo(novo_dado, aux.proximo)
+        self.tamanho = self.tamanho + 1
+        return True
