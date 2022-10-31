80eafad20277a5f988c5371ac07b46b81d12975f
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-10-31 09:46:58 -0300

    Update Main_lista.py

--- Lista/Main_lista.py
+++ Lista/Main_lista.py
@@ -20,3 +20,15 @@ print("Lista contém um único elemento:", lista)
 nodo_anterior = lista.cabeca
 lista.insereFim(nodo_anterior, 10)
 print("Inserindo um novo elemento depois de um outro:", lista)
+
+lista = Lista()
+for i in range(6):
+    lista.insereInicio(i)
+print("Lista:", lista)
+
+for i in range(6, -3, -2):
+    elemento = lista.busca(i)
+    if elemento:
+        print("Elemento {0} presente na lista!".format(i))
+    else:
+        print("Elemento {0} não encontrado.".format(i))
