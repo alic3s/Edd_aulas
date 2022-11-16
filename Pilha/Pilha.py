bfdb01c0a38e9558e80e5b6281ac1a6e225d7a01
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-11-16 12:14:12 -0300

    Update Pilha.py

--- Pilha/Pilha.py
+++ Pilha/Pilha.py
@@ -1,10 +1,10 @@
 class Nodo:
     def __init__(self, dado = 0, nodo_anterior = None):
         self.dado = dado
-        self.nodo_anterior = nodo_anterior
+        self.anterior = nodo_anterior
     
     def __repr__(self):
-        return f'{self.dado} -> {self.nodo_anterior}'
+        return f'{self.anterior} -> {self.dado}'
 
 class Pilha:
     def __init__(self):
@@ -27,14 +27,30 @@ class Pilha:
         self.tamanho += 1
 
     def remove(self):
-        removido = self.topo.dado
-        self.topo = self.topo.anterior
-        self.tamanho -= 1
-        return removido
+        if (self.vazia()):
+            print('Impossível remover valor de pilha vazia')
+        else:
+            self.tamanho -= 1
+            removido = self.topo.dado
+            self.topo = self.topo.anterior
+            return removido
+    
+    def compara(self, pilha2):
+        if self.tamanho != pilha2.tamanho:
+            return False
+        else:
+            no_atual1 = self.topo
+            no_atual2 = pilha2.topo
+            while(no_atual1 != None):
+                if(no_atual1.dado != no_atual2.dado):
+                    return False
+                no_atual1 = no_atual1.anterior
+                no_atual2 = no_atual2.anterior
+                return True
 
 
 #Exemplo de implementação de pilha
-pilha = Pilha()
+'''pilha = Pilha()
 
 for i in range(4):
     pilha.insere(i)
@@ -44,4 +60,22 @@ print(pilha.tamanho)
 
 while pilha.topo != None:
     pilha.remove()
-    print('Removendo o elemento que está no topo da pilha: ', pilha)
+    print('Removendo o elemento que está no topo da pilha: ', pilha)'''
+
+
+'''pilha = Pilha()
+pilha = []
+print("Informe os valores da pilha")
+print()
+# PREENCHENDO A PILHA
+for cont in range(7):
+    pilha.append(int(input('Informe o valor ' + str(cont + 1) + ' para a pilha: ')))
+    print(pilha)
+
+# ESVAZIANDO A PILHA
+while len(pilha) > 0:
+    pilha.pop()
+    print(pilha)
+
+# FINALIZANDO O PROGRAMA
+print("Fim...")'''
