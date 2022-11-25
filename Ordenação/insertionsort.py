9d0f43e06f3f7cf23a1a9ed61dd4738d5c519869
Alice Araújo <113319228+alic3s@users.noreply.github.com>
2022-11-25 20:40:23 -0300

    Update insertionsort.py

--- Ordenação/insertionsort.py
+++ Ordenação/insertionsort.py
@@ -1,12 +1,14 @@
 def insertionsort(lista):
     for  i in range(1, len(lista)):
+        escolhido = lista[i]
         j = i - 1
         while j >= 0 and lista[j] > lista[i]:
             aux = lista[i]
             lista[i] = lista[j]
             lista[j] = aux
             j -= 1
+        lista[j + 1] = escolhido
     return lista
 
-l = [5, 2, 9, 7]
+l = [5, 2, 9, 7, 12, 10, 25, 22]
 print(insertionsort(l))
